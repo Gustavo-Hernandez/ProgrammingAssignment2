167f7ba80fd3d272847fc18058cafe3dc52b3a16
Gustavo Hernández <47768780+Gustavo-Hernandez@users.noreply.github.com>
2021-01-13 19:06:08 -0600

    Completed Programming Assignment 2
    Added the requested functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inverse calculation and caching.
+## This script aims to reduce the computational costs
+## of recomputing the inverse of a matrix.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix generates a list of functions to
+## set and get a matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Getters and Setters.
+  inverse <- NULL
+  set <- function(nm){
+    x <<- nm
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) inverse <<- inv
+  getInverse <- function() inverse
+  
+  ##Return functions.
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve validates the existence of the inverse
+## of a matrix, if nonexistent, it is calculated,
+## assigned and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  # Search for cached inverse
+  inverse <- x$getInverse()
+  if(!is.null(inverse)){
+    message("Returning cached inverse ...")
+    return(inverse)
+  }
+  
+  #Calculate inverse
+  mt <- x$get()
+  localInverse <- solve(mt)
+  
+  #Assign Calculated Inverse
+  x$setInverse(localInverse)
+  
+  localInverse
 }
